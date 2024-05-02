378634d1bf62ec4d1619a63429b9df47b58fa208
code-xhyun <shhong@taling.me>
2024-05-03 01:02:16 +0900

    feat: add getJobsRepo method to Pulse class

--- src/pulse/jobs.ts
+++ src/pulse/jobs.ts
@@ -3,7 +3,12 @@ import { Pulse } from '.';
 import { Job } from '../job';
 import { createJob } from '../utils';
 
+export const getJobsRepo = async function (this: Pulse, query = {}, sort = {}, limit = 0, skip = 0) {
+  return await this._collection.find(query).sort(sort).limit(limit).skip(skip).toArray();
+};
+
 export type JobsMethod = (query?: Filter<any>, sort?: Sort | string, limit?: number, skip?: number) => Promise<Job[]>;
+
 /**
  * Finds all jobs matching 'query'
  * @name Pulse#jobs
@@ -15,7 +20,6 @@ export type JobsMethod = (query?: Filter<any>, sort?: Sort | string, limit?: num
  * @returns resolves when fails or passes
  */
 export const jobs: JobsMethod = async function (this: Pulse, query = {}, sort = {}, limit = 0, skip = 0) {
-  const result = await this._collection.find(query).sort(sort).limit(limit).skip(skip).toArray();
-
+  const result = await getJobsRepo.call(this, query, sort, limit, skip);
   return result.map((job: any) => createJob(this, job));
 };
